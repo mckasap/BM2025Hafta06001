68676a4bf25c67ba6e8353e7dcd4b5e3a1efb192
mckasap <mckasap@gmail.com>
2025-03-24 11:34:52 +0300

    ikinci yükleme

--- app/src/main/java/com/example/bm2025hafta06001/MainActivity.java
+++ app/src/main/java/com/example/bm2025hafta06001/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bm2025hafta06001;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,20 +16,23 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public class myTask extends AsyncTask<String, String, String> {
+    public class myTask extends AsyncTask<String, String, String[][]> {
         @Override
-        protected String doInBackground(String... strings) {
+        protected String[][] doInBackground(String... strings) {
+            String degerler[][]= new String[4][2];
             try {
                 Document doc = Jsoup.connect(strings[0]).get();
                 String title = doc.title();
                 Elements Currencies = doc.select("div.home-currency span.home-currency__type");
                 Elements Amounts = doc.select("div.home-currency strong.home-currency__val");
                 for (int i = 0; i < Currencies.size(); i++) {
-                    Log.d("Currency", Currencies.get(i).text());
-                    Log.d("Amount", Amounts.get(i).text());
+                    degerler[i][0] = Currencies.get(i).text();
+                    degerler[i][1] = Amounts.get(i).text();
                 }
 
                 Log.d("Title",title);
@@ -37,22 +41,31 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-            return null;
+            return degerler;
         }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        setContentView(R.layout.activity_main);
+        TextView tvDolor = findViewById(R.id.tvDolor);
+        TextView tvEuro = findViewById(R.id.tvEuro);
+        TextView tvBIST = findViewById(R.id.tvBIST);
+        TextView tvAltın = findViewById(R.id.tvAltın);
         myTask task = new myTask();
-        task.execute("https://www.milliyet.com.tr");
+        try {
+       String[][] degerler =    task.execute("https://www.milliyet.com.tr").get();
+            tvDolor.setText(degerler[0][0]+" "+degerler[0][1]);
+            tvEuro.setText(degerler[1][0]+" "+degerler[1][1]);
+            tvBIST.setText(degerler[2][0]+" "+degerler[2][1]);
+            tvAltın.setText(degerler[3][0]+" "+degerler[3][1]);
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
